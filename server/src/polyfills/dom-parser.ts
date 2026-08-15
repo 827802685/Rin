85a069d1a77f33f330fc7542ffe6dfe56c9b1d7b
zjkl <827802685@qq.com>
2026-08-15 18:53:18 +0800

    fix: polyfill Node global for aws-sdk xml parsing

--- server/src/polyfills/dom-parser.ts
+++ server/src/polyfills/dom-parser.ts
@@ -1,5 +1,9 @@
-import { DOMParser } from "@xmldom/xmldom";
+import { DOMParser, Node } from "@xmldom/xmldom";
 
 if (typeof globalThis.DOMParser === "undefined") {
   globalThis.DOMParser = DOMParser as unknown as typeof globalThis.DOMParser;
+}
+
+if (typeof (globalThis as { Node?: unknown }).Node === "undefined") {
+  (globalThis as { Node?: unknown }).Node = Node as unknown as typeof globalThis.Node;
 }
